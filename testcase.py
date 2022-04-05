969c4ff8f5ae94505edf312849514f2856726c69
Saikiran Nuthakki <62567277+saikiran-nuthakki@users.noreply.github.com>
2022-04-04 22:44:32 -0700

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -4,16 +4,16 @@ import unittest
 class Testhw2(unittest.TestCase):
 
     def test_num_placements_all(self):
-        self.assertEqual(main_solver.num_placements_all(4), 1820)
+        self.assertEqual(solution.num_placements_all(4), 1820)
 
     def test_num_placements_one_per_row(self):
-        self.assertEqual(main_solver.num_placements_one_per_row(4), 256)
+        self.assertEqual(solution.num_placements_one_per_row(4), 256)
 
     def test_n_queens_valid(self):
-        self.assertEqual(main_solver.n_queens_valid([0, 2]), True)
-        self.assertEqual(main_solver.n_queens_valid([0, 3, 1]), True)
+        self.assertEqual(solution.n_queens_valid([0, 2]), True)
+        self.assertEqual(solution.n_queens_valid([0, 3, 1]), True)
 
     def test_n_queens_solutions(self):
-        solution = main_solver.n_queens_solutions(4)
-        self.assertEqual(next(solution), [1, 3, 0, 2])
-        self.assertEqual(len(list(main_solver.n_queens_solutions(8))), 92)
+        sol = solution.n_queens_solutions(4)
+        self.assertEqual(next(sol), [1, 3, 0, 2])
+        self.assertEqual(len(list(solution.n_queens_solutions(8))), 92)
